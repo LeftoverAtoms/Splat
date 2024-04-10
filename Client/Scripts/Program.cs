ed0be0e05a4ac3da6cf9caf91b217ced84202f34
Adam Calvelage <adamjasoncalvelage@gmail.com>
2024-04-09 23:24:09 -0400

    Remove SDL Prefixes

--- Client/Scripts/Program.cs
+++ Client/Scripts/Program.cs
@@ -1,72 +1,75 @@
-﻿using SDL2;
+﻿global using static SDL2.SDL;
+global using static SDL2.SDL_ttf;
 using System;
 
 namespace Splat;
 
 class Program
 {
-	static nint Window;
-	static nint Renderer;
+	public static IntPtr Window;
+	public static IntPtr Renderer;
+
+	public static IntPtr Font;
 
 	static void Main(string[] args)
 	{
+		if (!SetupSDL())
+			return;
+
 		// Connect a client to the server.
 		var client = new Client("Adam");
 		client.Connect("127.0.0.1", 1234);
 
-		SetupSDL();
-
-		SDL_ttf.TTF_Init();
-		IntPtr font = SDL_ttf.TTF_OpenFont("Roboto-Regular.ttf", 24);
-		if (font == IntPtr.Zero)
-		{
-			Console.WriteLine("Failed to load font");
-			return;
-		}
-
-		SDL.SDL_Color textColor = new SDL.SDL_Color { r = 255, g = 255, b = 255 };
-
-		IntPtr surface = SDL_ttf.TTF_RenderText_Solid(font, client.Data.Name, textColor);
-		if (surface == IntPtr.Zero)
-		{
-			Console.WriteLine("Failed to render text surface");
-			return;
-		}
-
-		IntPtr texture = SDL.SDL_CreateTextureFromSurface(Renderer, surface);
-		if (texture == IntPtr.Zero)
-		{
-			Console.WriteLine("Failed to create texture from surface");
-			return;
-		}
-
-		SDL.SDL_FreeSurface(surface);
-
-		SDL.SDL_Rect destRect = new SDL.SDL_Rect { x = 100, y = 100, w = 64, h = 32 };
-
-		SDL.SDL_RenderCopy(Renderer, texture, IntPtr.Zero, ref destRect);
-
-		SDL.SDL_RenderPresent(Renderer);
 
 		while (true)
 		{
-			while (SDL.SDL_PollEvent(out SDL.SDL_Event e) == 1)
+			while (SDL_PollEvent(out SDL_Event e) == 1)
 			{
 				switch (e.type)
 				{
-					case SDL.SDL_EventType.SDL_QUIT:
+					case SDL_EventType.SDL_QUIT:
 					return;
 				}
 			}
 		}
 	}
 
-	static void SetupSDL()
+	static bool SetupSDL()
 	{
-		SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+		if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+		{
+			Console.WriteLine(SDL_GetError());
+			return false;
+		}
 
-		Window = SDL.SDL_CreateWindow("Splat", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED, 1280, 720, SDL.SDL_WindowFlags.SDL_WINDOW_MAXIMIZED);
+		if (TTF_Init() != 0)
+		{
+			Console.WriteLine(SDL_GetError());
+			return false;
+		}
 
-		Renderer = SDL.SDL_CreateRenderer(Window, 0, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+		Window = SDL_CreateWindow("Splat", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WindowFlags.SDL_WINDOW_MAXIMIZED);
+		if (Window == IntPtr.Zero)
+		{
+			Console.WriteLine(SDL_GetError());
+			return false;
+		}
+
+		Renderer = SDL_CreateRenderer(Window, 0, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+		if (Renderer == IntPtr.Zero)
+		{
+			Console.WriteLine(SDL_GetError());
+			return false;
+		}
+
+		Font = TTF_OpenFont("Roboto-Regular.ttf", 24);
+		if (Font == IntPtr.Zero)
+		{
+			Console.WriteLine(SDL_GetError());
+			return false;
+		}
+
+		// Successfully initialized!
+		return true;
 	}
 }
