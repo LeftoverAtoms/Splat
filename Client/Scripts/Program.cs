5c731550cc7169caf08120edad5ab4b14c6f8fe2
Adam Calvelage <adamjasoncalvelage@gmail.com>
2024-04-10 23:08:10 -0400

    Move Game Loop into Separate Functions

--- Client/Scripts/Program.cs
+++ Client/Scripts/Program.cs
@@ -15,86 +15,55 @@ public class Program
 
 	public static IntPtr Font { get; private set; }
 
-	static void Main(string[] args)
+	static bool running = true;
+
+	static void Main()
 	{
-		if (!SetupSDL())
+		// Initialize sdl.
+		if (!SetupSDL(title: "Splat", font: "Roboto-Regular"))
 			return;
 
-		// Create textfield.
-		new TextField(128, 128, "TextField 1");
-		new TextField(512, 128, "TextField 2");
+		// Create textfields.
+		_ = new TextField(128, 128, "TextField 1");
+		_ = new TextField(512, 128, "TextField 2");
 
-		bool isRunning = true;
-
-		// Gameloop
-		while (isRunning)
+		// Execute gameloop.
+		while (running)
 		{
-			Cursor.State = SDL_RELEASED;
-
-			// Handle all events within the queue before continuing.
-			while (SDL_PollEvent(out var evt) != 0)
-			{
-				Cursor.Selection?.OnEvent(evt);
-
-				switch (evt.type)
-				{
-					case SDL_EventType.SDL_MOUSEMOTION:
-					{
-						Cursor.Position = new Vector2(evt.motion.x, evt.motion.y);
-						break;
-					}
-
-					case SDL_EventType.SDL_MOUSEBUTTONDOWN:
-					{
-						Cursor.State = evt.button.state;
-						break;
-					}
-
-					case SDL_EventType.SDL_QUIT:
-					{
-						isRunning = false;
-						break;
-					}
-				}
-			}
-
-			// Update
-			foreach (var entity in Entities)
-			{
-				entity.Update();
-			}
-
-			// Render
-			SDL_RenderClear(Program.Renderer);
-			foreach (var entity in Entities)
-			{
-				entity.Render();
-			}
-			SDL_RenderPresent(Program.Renderer);
+			Input();
+			Update();
+			Render();
 		}
+
+		// Exit application.
+		Quit();
 	}
 
-	static bool SetupSDL()
+	static bool SetupSDL(string title, string font)
 	{
+		// Init sdl.
 		if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 		{
 			Console.WriteLine(SDL_GetError());
 			return false;
 		}
 
+		// Init font rendering.
 		if (TTF_Init() != 0)
 		{
 			Console.WriteLine(SDL_GetError());
 			return false;
 		}
 
-		Window = SDL_CreateWindow("Splat", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WindowFlags.SDL_WINDOW_MAXIMIZED);
+		// Create window.
+		Window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WindowFlags.SDL_WINDOW_MAXIMIZED);
 		if (Window == IntPtr.Zero)
 		{
 			Console.WriteLine(SDL_GetError());
 			return false;
 		}
 
+		// Create renderer.
 		Renderer = SDL_CreateRenderer(Window, 0, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
 		if (Renderer == IntPtr.Zero)
 		{
@@ -102,14 +71,76 @@ public class Program
 			return false;
 		}
 
-		Font = TTF_OpenFont("Roboto-Regular.ttf", 24);
+		// Open font.
+		Font = TTF_OpenFont($"{font}.ttf", 24);
 		if (Font == IntPtr.Zero)
 		{
 			Console.WriteLine(SDL_GetError());
 			return false;
 		}
 
-		// Successfully initialized!
+		// Success!
 		return true;
+	}
+
+	static void Input()
+	{
+		Cursor.State = SDL_RELEASED;
+
+		// Handle all events within the queue before continuing.
+		while (SDL_PollEvent(out var evt) != 0)
+		{
+			Cursor.Selection?.OnEvent(evt);
+
+			switch (evt.type)
+			{
+				case SDL_EventType.SDL_MOUSEMOTION:
+				{
+					Cursor.Position = new Vector2(evt.motion.x, evt.motion.y);
+					break;
+				}
+
+				case SDL_EventType.SDL_MOUSEBUTTONDOWN:
+				{
+					Cursor.State = evt.button.state;
+					break;
+				}
+
+				case SDL_EventType.SDL_QUIT:
+				{
+					running = false;
+					break;
+				}
+			}
+		}
+	}
+	static void Update()
+	{
+		foreach (var entity in Entities)
+		{
+			entity.Update();
+		}
+	}
+	static void Render()
+	{
+		if (SDL_RenderClear(Renderer) != 0)
+		{
+			Console.WriteLine(SDL_GetError());
+			return;
+		}
+
+		foreach (var entity in Entities)
+		{
+			entity.Render();
+		}
+
+		SDL_RenderPresent(Renderer);
+	}
+
+	static void Quit()
+	{
+		SDL_DestroyWindow(Window);
+		SDL_DestroyRenderer(Renderer);
+		TTF_CloseFont(Font);
 	}
 }
